9873c8c3559f64bf7cbae62d2b85959270dc9d6d
Tobias Kunze <r@rixx.de>
2023-03-02 20:21:34 +0100

    Document simple graphql API

---
+++ {{cookiecutter.repo_name}}/{{cookiecutter.module_name}}/graphql.py
@@ -0,0 +1,10 @@
+# from netbox.graphql.types import NetBoxObjectType
+
+# from . import models, filters
+
+
+# class MyModelType(NetBoxObjectType):
+#     class Meta:
+#         model = models.MyModel
+#         fields = '__all__'
+#         filterset_class = filters.MyModelFilterSet
